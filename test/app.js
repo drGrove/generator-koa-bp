37a5fdd1e11524dd72395289c1289a678f4bbd09
Danny Grove <danny@drgrovellc.com>
2016-07-01 23:58:23 -0700

    Fix tests to align with new asset structure

--- test/app.js
+++ test/app.js
@@ -5,74 +5,97 @@ var helpers = require('yeoman-test');
 var assets = require('./assets.json');
 
 describe('generator-koa-bp:app', function() {
-  before(function(done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({appName: 'testApi'})
-      .on('end', done);
-  });
+  describe('with oAuth', function() {
+    before(function(done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          appName: 'testApi',
+          includeOAuthProviders: true
+        })
+        .toPromise()
+        .then(function() {
+          done();
+        });
+    });
 
-  it('creates metafiles', function() {
-    assert.file(assets.metaFiles);
-  });
+    it('creates metafiles', function() {
+      assert.file(assets.metaFiles);
+    });
 
-  it('creates swagger files', function() {
-    assert.file(assets.useSwagger);
-  });
+    it('creates swagger files', function() {
+      assert.file(assets.useSwagger);
+    });
 
-  it('creates lib files', function() {
-    assert.file(assets.libFiles);
-  });
+    it('creates lib files', function() {
+      assert.file(assets.libFiles);
+    });
 
-  it('creates route files', function() {
-    assert.file(assets.routeFiles);
-  });
+    it('creates route files', function() {
+      assert.file(assets.routeFiles);
+    });
 
-  it('creates model files', function() {
-    assert.file(assets.modelFiles);
-  });
+    it('creates oauth route files', function() {
+      assert.file(assets.oAuthFiles);
+    });
 
-  it('creates test files', function() {
-    assert.file(assets.testFiles);
-  });
+    it('creates model files', function() {
+      assert.file(assets.modelFiles);
+    });
 
-  it('creates gulp files', function() {
-    assert.file(assets.gulpFiles);
+    it('creates test files', function() {
+      assert.file(assets.testFiles);
+    });
+
+    it('creates gulp files', function() {
+      assert.file(assets.gulpFiles);
+    });
+
+    it('creates database files', function() {
+      assert.file(assets.mysqlFiles);
+    });
+  });
+  describe('with named application', function() {
+    before(function(done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withArguments(['testApi2'])
+        .withPrompts({})
+        .toPromise()
+        .then(function() {
+          done();
+        });
+    });
+
+    it('creates metafiles', function() {
+      assert.file(assets.metaFiles);
+    });
+
+    it('creates swagger files', function() {
+      assert.file(assets.useSwagger);
+    });
+
+    it('creates lib files', function() {
+      assert.file(assets.libFiles);
+    });
+
+    it('creates route files', function() {
+      assert.file(assets.routeFiles);
+    });
+
+    it('creates model files', function() {
+      assert.file(assets.modelFiles);
+    });
+
+    it('creates test files', function() {
+      assert.file(assets.testFiles);
+    });
+
+    it('creates gulp files', function() {
+      assert.file(assets.gulpFiles);
+    });
+
+    it('creates database files', function() {
+      assert.file(assets.mysqlFiles);
+    });
   });
 });
 
-describe('generator-koa-bp:app testApi2', function() {
-  before(function(done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withArguments(['testApi2'])
-      .withPrompts({})
-      .on('end', done);
-  });
-
-  it('creates metafiles', function() {
-    assert.file(assets.metaFiles);
-  });
-
-  it('creates swagger files', function() {
-    assert.file(assets.useSwagger);
-  });
-
-  it('creates lib files', function() {
-    assert.file(assets.libFiles);
-  });
-
-  it('creates route files', function() {
-    assert.file(assets.routeFiles);
-  });
-
-  it('creates model files', function() {
-    assert.file(assets.modelFiles);
-  });
-
-  it('creates test files', function() {
-    assert.file(assets.testFiles);
-  });
-
-  it('creates gulp files', function() {
-    assert.file(assets.gulpFiles);
-  });
-});
