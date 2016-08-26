0aefb925223bfb92fc78fa93cff7aab3c073dcfc
Danny Grove <danny@drgrovellc.com>
2016-08-26 00:26:38 -0700

    Add test for route creation

---
+++ test/addRoute.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+var path = require('path');
+var spawn = require('child_process').spawn;
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+var assets = require('./assets.json');
+
+describe('Make a new custom route', function() {
+  var tempDir;
+  before(function(done) {
+    helpers.run(path.join(__dirname, '../generators/route'))
+      .withPrompts({
+        route: 'users/tests'
+      })
+      .toPromise()
+      .then(function(dir) {
+        tempDir = dir;
+        done();
+      })
+      .catch(function(err) {
+        console.error('ERROR: ', err);
+      });
+  });
+
+  after(function(done) {
+    let deleteTmp = spawn
+      ( 'rm'
+        , [ '-r'
+        , `/tmp/${tempDir}`
+        ]
+      );
+    deleteTmp.on('close', function() {
+      done();
+    });
+  });
+
+  it('Should create the route folder', function() {
+    assets.file('routes/users/tests/index.js');
+  });
+})
